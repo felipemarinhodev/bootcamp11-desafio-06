2bb1c4eb480d555cfa2bd766ab009885f24a6652
Felipe Marinho <felipmarinho@gmail.com>
2020-05-04 05:13:26 -0300

    Adding create transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,33 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  try {
+    const { title, value, type, category } = request.body;
+    const createTransactionService = new CreateTransactionService();
+    const transaction = await createTransactionService.execute({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    return response.send(transaction);
+  } catch (error) {
+    return response
+      .status(400)
+      .json({ message: 'Erro ao salvar uma transaction', error });
+  }
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,44 @@
+import { getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  value: string;
+  type: string;
+  category: number;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRespository = getRepository(Transaction);
+
+    const categoryRepository = getRepository(Category);
+
+    const categorySaved = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categorySaved) {
+      throw new Error('Does not exist category informed.');
+    }
+
+    const transaction = transactionRespository.create({
+      title,
+      value,
+      type,
+      category: categorySaved.id,
+    });
+
+    await transactionRespository.save(transaction);
+
+    return transaction;
   }
 }
 
